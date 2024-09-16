1ecb78e88b85b772e13ab081f74b7d1470e7faf7
BrianBi <brayanbigabriel@gmail.com>
2024-09-16 16:29:43 -0400

    separación del puerto 3306

--- config/db.js
+++ config/db.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: '154.56.48.204',  // Dirección IP sin el puerto
+    host: '154.56.48.204',
+    port: 3306,  // Usa el puerto 3306 si no es personalizado
     user: 'u300220289_adm_pablo',
     password: 'Hola_123*321',
-    database: 'u300220289_diego_db',
-    port: 3006  // Puerto en una propiedad separada
+    database: 'u300220289_diego_db'
 });
+
 
 connection.connect((err) => {
     if (err) {
