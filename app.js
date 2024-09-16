63ecd8093b439e07604f8b5ffa33e34f1bb56628
BrianBi <brayanbigabriel@gmail.com>
2024-09-16 16:41:50 -0400

    modificacion app.js puertos

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require('express'); 
 const datosRoutes = require('./routes/urls');
 const app = express();
-const port = 3004;
+const port = 3004; // Aquí defines "port", no "PORT"
 
 app.use(express.json());
 
@@ -10,9 +10,9 @@ app.use('/', datosRoutes);
 app.use(express.static('public'));
 app.use('/static', express.static('static'));
 
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
+// Cambia "PORT" por "port" en la función app.listen
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Servidor corriendo en http://0.0.0.0:${port}`);
 });
-
 
 module.exports = app;
