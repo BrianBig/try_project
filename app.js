88532059b4e2405f4aea83763fe37a67523d794d
BrianBi <brayanbigabriel@gmail.com>
2024-09-16 16:09:35 -0400

    separación del puerto 3006

--- app.js
+++ app.js
@@ -3,17 +3,13 @@ const datosRoutes = require('./routes/urls');
 const app = express();
 const port = 3003;
 
-// Middleware para interpretar JSON en el cuerpo de las solicitudes
 app.use(express.json());
 
-// Configuración de las rutas
 app.use('/', datosRoutes);
 
-// Configuración de archivos estáticos
 app.use(express.static('public'));
 app.use('/static', express.static('static'));
 
-// Iniciar el servidor
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
 });
--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: '154.56.48.204:3006',
+    host: '154.56.48.204',  // Dirección IP sin el puerto
     user: 'u300220289_adm_pablo',
     password: 'Hola_123*321',
-    database: 'u300220289_diego_db'
+    database: 'u300220289_diego_db',
+    port: 3006  // Puerto en una propiedad separada
 });
 
 connection.connect((err) => {
